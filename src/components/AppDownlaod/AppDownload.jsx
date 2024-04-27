1f2e239624b43464ee1cc416d15de1ccb2724d89
Shivam Jadhav <127479563+shivam7695@users.noreply.github.com>
2024-04-27 22:17:25 +0530

    Update AppDownload.jsx

--- src/components/AppDownlaod/AppDownload.jsx
+++ src/components/AppDownlaod/AppDownload.jsx
@@ -6,8 +6,8 @@ const AppDownload = () => {
     <div className='app-downlaod' id='app-downlaod' >
         <p>For Better Experience Download  <br /> Foodee App</p>
         <div className="app-download-platform">
-            <img src="playstore.png" alt="" />
-            <img src="appstore.png" alt="" />
+            <img src="https://imgs.search.brave.com/JW7qv3zpNORbj5FNX7uC47RIUTEyFj1Z6LSODQZ8CPs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZXRy/aWthbC5pby9ibG9n/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIw/LzEwL25ldy1nb29n/bGUtcGxheS1zdG9y/ZS1tb2JpbGUtMTI4/MHg3MjAtMS0xMDI0/eDU3Ni5qcGc" alt="" />
+            <img src="https://imgs.search.brave.com/tPKe4ZmAvd8BsM9249TT9RNPdQwbWy8qxkopiiuVmIY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cGljdHVyZXRoaXNh/aS5jb20vc3RhdGlj/L2ltYWdlcy9hcHBf/c3RvcmVfZW4ucG5n" alt="" />
         </div>
     </div>
   )
